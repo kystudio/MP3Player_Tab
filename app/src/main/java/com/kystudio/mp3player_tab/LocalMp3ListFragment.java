6a77b74384721e9dcd908bd88ae6fb10df8b4401
kystudio <yuan012308@hotmail.com>
2016-10-08 01:18:33 +0800

    Activity -->Fragment

--- app/src/main/java/com/kystudio/mp3player_tab/LocalMp3ListFragment.java
+++ app/src/main/java/com/kystudio/mp3player_tab/LocalMp3ListFragment.java
@@ -1,16 +1,29 @@
 package com.kystudio.mp3player_tab;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.kystudio.model.Mp3Info;
+import com.kystudio.utils.FileUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link ListFragment} subclass.
  */
-public class LocalMp3ListFragment extends Fragment {
+public class LocalMp3ListFragment extends ListFragment {
+    private List<Mp3Info> mp3Infos = null;
+
     public LocalMp3ListFragment() {
         // Required empty public constructor
     }
@@ -24,4 +37,35 @@ public class LocalMp3ListFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onResume() {
+        FileUtils fileUtils = new FileUtils();
+        mp3Infos = fileUtils.getMp3Files("mp3/");
+        List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        for (Iterator iterator = mp3Infos.iterator(); iterator.hasNext(); ) {
+            Mp3Info mp3Info = (Mp3Info) iterator.next();
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("mp3_name", mp3Info.getMp3Name());
+            map.put("mp3_size", mp3Info.getMp3Size());
+
+            list.add(map);
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this.getActivity(), list, R.layout.mp3info_item, new String[]{"mp3_name", "mp3_size"}, new int[]{R.id.mp3_name, R.id.mp3_size});
+        setListAdapter(simpleAdapter);
+
+        super.onResume();
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (mp3Infos != null) {
+            Mp3Info mp3Info = mp3Infos.get(position);
+            Intent intent = new Intent();
+            intent.putExtra("mp3Info", mp3Info);
+            intent.setClass(this.getActivity(), PlayerActivity.class);
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/kystudio/mp3player_tab/RemoteMp3ListFragment.java
+++ app/src/main/java/com/kystudio/mp3player_tab/RemoteMp3ListFragment.java
@@ -1,18 +1,37 @@
 package com.kystudio.mp3player_tab;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.kystudio.download.HttpDownloader;
+import com.kystudio.model.Mp3Info;
+import com.kystudio.mp3player_tab.service.DownloadService;
+import com.kystudio.xml.Mp3ListContentHandler;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.SAXParserFactory;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link ListFragment} subclass.
  */
-public class RemoteMp3ListFragment extends Fragment {
-
+public class RemoteMp3ListFragment extends ListFragment {
+    private List<Mp3Info> mp3Infos = null;
 
     public RemoteMp3ListFragment() {
         // Required empty public constructor
@@ -24,8 +43,81 @@ public class RemoteMp3ListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_remote_mp3_list, container, false);
-
+        updateListView();
         return rootView;
     }
 
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Mp3Info mp3Info = mp3Infos.get(position);
+
+        Intent intent = new Intent();
+        intent.putExtra("mp3Info", mp3Info);
+        intent.setClass(this.getActivity(), DownloadService.class);
+        getActivity().startService(intent);
+        super.onListItemClick(l, v, position, id);
+    }
+
+    private void updateListView() {
+        downloadXML(AppConstant.URL.BASE_URL + "resource.xml");
+    }
+
+    private void downloadXML(String urlStr) {
+        MyDownloadThread md = new MyDownloadThread(urlStr);
+        md.start();
+    }
+
+    private List<Mp3Info> parse(String xmlStr) {
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        List<Mp3Info> infos = new ArrayList<Mp3Info>();
+
+        try {
+            XMLReader xmlReader = saxParserFactory.newSAXParser().getXMLReader();
+            Mp3ListContentHandler mp3ListContentHandler = new Mp3ListContentHandler(infos);
+            xmlReader.setContentHandler(mp3ListContentHandler);
+            xmlReader.parse(new InputSource(new StringReader(xmlStr)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return infos;
+    }
+
+    private SimpleAdapter buildSimpleAdapter(List<Mp3Info> mp3Infos) {
+        List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        for (Iterator iterator = mp3Infos.iterator(); iterator.hasNext(); ) {
+            Mp3Info mp3Info = (Mp3Info) iterator.next();
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("mp3_name", mp3Info.getMp3Name());
+            map.put("mp3_size", mp3Info.getMp3Size());
+            list.add(map);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this.getActivity(), list, R.layout.mp3info_item, new String[]{"mp3_name", "mp3_size"}, new int[]{R.id.mp3_name, R.id.mp3_size});
+
+        return simpleAdapter;
+    }
+
+    class MyDownloadThread extends Thread {
+        private String urlStr;
+
+        public MyDownloadThread(String urlStr) {
+            this.urlStr = urlStr;
+        }
+
+        @Override
+        public void run() {
+            HttpDownloader httpDownloader = new HttpDownloader();
+            String result = httpDownloader.downloadTxt(urlStr);
+
+            mp3Infos = parse(result);
+            // 更新UI
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    SimpleAdapter simpleAdapter = buildSimpleAdapter(mp3Infos);
+                    setListAdapter(simpleAdapter);
+                }
+            });
+        }
+    }
 }
